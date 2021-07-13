4c42d63974b5294588a7160c6d9a0dc228f97407
bandriychuk <andriychuk23@gmail.com>
2021-07-13 09:40:33 +0300

    Added Movie

--- src/main/java/com/java/spring/entity/Movie.java
+++ src/main/java/com/java/spring/entity/Movie.java
@@ -2,11 +2,22 @@ package com.java.spring.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Entity
+//@Table(name = "MOVIE_TABLE") // - @Table - дозволяє створити таблицю з іменем яке нам потрібно
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false, length = 100)
     private String title;
     private int duration;
+
 }
