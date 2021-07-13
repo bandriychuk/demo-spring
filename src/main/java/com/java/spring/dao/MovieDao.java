18074cf152b077229c7f511405f5112e88f92cf4
bandriychuk <andriychuk23@gmail.com>
2021-07-13 09:39:49 +0300

    Added Movie DAO

---
+++ src/main/java/com/java/spring/dao/MovieDao.java
@@ -0,0 +1,8 @@
+package com.java.spring.dao;
+
+import com.java.spring.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieDao extends JpaRepository<Movie,Integer> {
+//    List<Movie> movies = new ArrayList<>();
+}
