ee0d91d424cde61aed0ba64d451ab43b5e265a23
bandriychuk <andriychuk23@gmail.com>
2021-07-13 09:40:09 +0300

    Added Movie Service

---
+++ src/main/java/com/java/spring/service/MovieService.java
@@ -0,0 +1,17 @@
+package com.java.spring.service;
+
+import com.java.spring.entity.Movie;
+
+import java.util.List;
+
+public interface MovieService {
+
+    List<Movie> getAllMovies();
+
+    void deleteMovie(int id);
+
+    Movie updateMovie(int id, Movie movie);
+
+    Movie createMovie(Movie movie);
+
+}
