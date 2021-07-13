a07fc5d123301f607c50f59f87e98d863bbcafaf
bandriychuk <andriychuk23@gmail.com>
2021-07-13 09:40:16 +0300

    Added Movie Controller

--- src/main/java/com/java/spring/controller/MovieController.java
+++ src/main/java/com/java/spring/controller/MovieController.java
@@ -1,62 +1,64 @@
 package com.java.spring.controller;
 
 import com.java.spring.entity.Movie;
+import com.java.spring.service.MovieServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController// Щоб клас почав, хендлити реквести ставимо аннотацію @RestController
-
+@RequiredArgsConstructor
 public class MovieController {
 
-    private List<Movie> movies = new ArrayList<>();
 
-    {
-        movies.add(new Movie(1, "Start Wars: New Hope", 190));
-        movies.add(new Movie(2, "LoTR: RoTK", 234));
-    }
+    //Обєкти в Spring називаються Bean;
+
+    @Autowired
+    private MovieServiceImpl movieService;
 
     //    @RequestMapping(value = "/movie", method = RequestMethod.GET)
     @GetMapping(value = "/movie")
     public List<Movie> getMovies() {
-        return movies;
+        return movieService.getAllMovies();
+
     }
 
     @PostMapping(value = "/movie")
     @ResponseStatus(HttpStatus.CREATED)
     public Movie insertMovie(@RequestBody Movie movie) {
-        movies.add(movie);
-        return movie;
+        return movieService.createMovie(movie);
     }
 
     @PutMapping(value = "/movie/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public Movie updateMovie(@PathVariable int id, @RequestBody Movie movie) {
-        final Optional<Movie> first = movies.stream()
-                .filter(m -> m.getId() == id)
-                .findFirst(); // - Шукаємо в стрімі, чи є обєкт з ід
-        final Movie movieInList =
-                first.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No movie found")); // Якщо, немає ід тоді повертаємо 404
+//        final Optional<Movie> first = movies.stream()
+//                .filter(m -> m.getId() == id)
+//                .findFirst(); // - Шукаємо в стрімі, чи є обєкт з ід
+//        final Movie movieInList =
+//                first.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No movie found")); // Якщо, немає ід тоді повертаємо 404
+//
+//        final int index = movies.indexOf(movieInList);
+//        movie.setId(id);
+//        movies.set(index, movie); // - Якщо, є ід тоді оновлюємо обєкт
+//        return movie;
+        return movieService.updateMovie(id, movie);
 
-        final int index = movies.indexOf(movieInList);
-        movie.setId(id);
-        movies.set(index, movie); // - Якщо, є ід тоді оновлюємо обєкт
-        return movie;
     }
 
     @DeleteMapping(value = "/movie/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteMovie(@PathVariable int id) {
-        final boolean isRemoved = movies.removeIf(movie -> movie.getId() == id);
-        if (isRemoved) {
-            System.out.println("Movie Removed");
-        } else {
-            System.out.println("No movie with such id");
-        }
+//        final boolean isRemoved = movies.removeIf(movie -> movie.getId() == id);
+//        if (isRemoved) {
+//            System.out.println("Movie Removed");
+//        } else {
+//            System.out.println("No movie with such id");
+//        }
+        movieService.deleteMovie(id);
     }
 
 
